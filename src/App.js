478cf05bad1c8453a7f52cc190f3c9b41020b1a9
Rob Shelly <robshelly89@gmail.com>
2017-12-03 20:18:00 +0000

    Minor change to demo CI/CD of app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          This is Just a Sample Node React App!.
+          This is just a Sample Node React App used for demo purposes!.
         </p>
       </div>
     );
